0f5f9274fe3cd15e8c5c000429621dfc86a49f89
pyzcool <najib.raihan@ostorlab.com>
2022-10-06 14:06:03 +0100

    Fix comments.

--- tests/zap_agent_test.py
+++ tests/zap_agent_test.py
@@ -13,6 +13,7 @@ def testAgentZap_whenDomainNameAsset_RunScan(scan_message, test_agent, mocker, a
         assert len(agent_mock) > 0
         assert agent_mock[0].selector == 'v3.report.vulnerability'
 
+
 def testAgentZap_whenDomainNameAssetAndUrlScope_RunScan(scan_message_2, test_agent_with_url_scope, mocker, agent_mock):
     """Tests running the agent and emitting vulnerabilities."""
     with (pathlib.Path(__file__).parent / 'zap-test-output.json').open('r', encoding='utf-8') as o:
@@ -22,6 +23,17 @@ def testAgentZap_whenDomainNameAssetAndUrlScope_RunScan(scan_message_2, test_age
         assert mock_scan.is_called_once_with('https://ostorlab.co')
         assert len(agent_mock) > 0
         assert agent_mock[0].selector == 'v3.report.vulnerability'
+
+
+def testAgentZap_whenDomainNameAssetAndUrlScope_NotRunScan(scan_message, test_agent_with_url_scope, mocker,
+                                                           agent_mock):
+    """Tests running the agent and emitting vulnerabilities."""
+    with (pathlib.Path(__file__).parent / 'zap-test-output.json').open('r', encoding='utf-8') as o:
+        mock_scan = mocker.patch('agent.zap_wrapper.ZapWrapper.scan', return_value=json.load(o))
+        test_agent_with_url_scope.start()
+        test_agent_with_url_scope.process(scan_message)
+        mock_scan.assert_not_called()
+
 
 def testAgentZap_whenLinkAsset_RunScan(scan_message_link, test_agent, mocker, agent_mock):
     """Tests running the agent and emitting vulnerabilities."""
