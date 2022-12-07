4e2d4b5c945d6b9cdf9e4b10b0a0c14f048ac914
mohsinenar <mnarmouche@gmail.com>
2022-12-07 11:02:37 +0100

    [skip ci] Add black formatter to GitHub workflow.

--- tests/result_parser_test.py
+++ tests/result_parser_test.py
@@ -9,14 +9,24 @@ from agent import result_parser
 
 def testParseResults_always_yieldsValidVulnerabilities():
     """Test proper parsing of Zap JSON output."""
-    with (pathlib.Path(__file__).parent / 'zap-test-output.json').open('r', encoding='utf-8') as o:
+    with (pathlib.Path(__file__).parent / "zap-test-output.json").open(
+        "r", encoding="utf-8"
+    ) as o:
         results = json.load(o)
         vulnz = list(result_parser.parse_results(results))
         assert len(vulnz) == 216
-        parsed_vulnz = [v for v in vulnz if v.entry.title == 'Absence of Anti-CSRF Tokens']
+        parsed_vulnz = [
+            v for v in vulnz if v.entry.title == "Absence of Anti-CSRF Tokens"
+        ]
         assert len(parsed_vulnz) == 11
         assert parsed_vulnz[0].entry.risk_rating == 4
-        assert 'http://projects.webappsec.org/Cross-Site-Request-Forgery' in parsed_vulnz[0].entry.references
-        assert 'Phase: Architecture and Design' in parsed_vulnz[0].entry.recommendation
-        assert 'action="/search" method="GET" role="search">' in parsed_vulnz[0].technical_detail
+        assert (
+            "http://projects.webappsec.org/Cross-Site-Request-Forgery"
+            in parsed_vulnz[0].entry.references
+        )
+        assert "Phase: Architecture and Design" in parsed_vulnz[0].entry.recommendation
+        assert (
+            'action="/search" method="GET" role="search">'
+            in parsed_vulnz[0].technical_detail
+        )
         assert parsed_vulnz[0].risk_rating == vuln_mixin.RiskRating.POTENTIALLY
